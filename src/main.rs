c4d04a0522d38fecf7e7d305e70f75496dc6de18
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-01-09 17:54:51 +0330

    minimal kernel with "Hello World" message

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
-#![no_std]
-#![no_main]
+#![no_std] // prevent link the standard library
+#![no_main] //disable all rust entry points
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
+// to be called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
+#[no_mangle] // do not mangle function name
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
