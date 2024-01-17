f6eb3795dcf286d3a39a19ad26a4e664b9d0e3fd
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-01-17 16:23:32 +0330

    hardware interrupts: timer, halt and keyboard

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,14 @@ use core::panic::PanicInfo;
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -59,7 +67,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!(" [failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
@@ -67,7 +75,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,14 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("It didn't crash");
-
-    loop {}
+    r_os::hlt_loop();
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    r_os::hlt_loop();
 }
 
 #[cfg(test)]
